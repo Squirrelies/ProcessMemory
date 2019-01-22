19029e8da9c8fbcf086862d24640007c2eb3f1d3
Travis J. Gutjahr <33809229+Squirrelies@users.noreply.github.com>
2019-01-22 14:29:23 -0600

    Initial Commit.

---
+++ ProcessMemory/PInvoke.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ProcessMemory
+{
+    internal static class PInvoke
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern UIntPtr OpenProcess(ProcessAccessFlags processAccess, bool bInheritHandle, uint processId);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern bool CloseHandle(UIntPtr hObject);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern bool ReadProcessMemory(UIntPtr hProcess, ulong lpBaseAddress, [Out] byte[] lpBuffer, int nSize, out IntPtr lpNumberOfBytesRead);
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern bool WriteProcessMemory(UIntPtr hProcess, ulong lpBaseAddress, byte[] lpBuffer, int nSize, out IntPtr lpNumberOfBytesWritten);
+
+        [DllImport("kernel32.dll")]
+        internal static extern void GetSystemInfo(out SYSTEM_INFO lpSystemInfo);
+
+        [DllImport("kernel32.dll")]
+        internal static extern int VirtualQueryEx(UIntPtr hProcess, UIntPtr lpAddress, out MEMORY_BASIC_INFORMATION64 lpBuffer, UIntPtr dwLength);
+
+        [Flags]
+        public enum AllocationProtect : uint
+        {
+            PAGE_NOACCESS = 0x00000001,
+            PAGE_READONLY = 0x00000002,
+            PAGE_READWRITE = 0x00000004,
+            PAGE_WRITECOPY = 0x00000008,
+            PAGE_EXECUTE = 0x00000010,
+            PAGE_EXECUTE_READ = 0x00000020,
+            PAGE_EXECUTE_READWRITE = 0x00000040,
+            PAGE_EXECUTE_WRITECOPY = 0x00000080,
+            PAGE_GUARD = 0x00000100,
+            PAGE_NOCACHE = 0x00000200,
+            PAGE_WRITECOMBINE = 0x00000400,
+            PAGE_ENCLAVE_UNVALIDATED = 0x20000000,
+            PAGE_TARGETS_INVALID = 0x40000000,
+            PAGE_TARGETS_NO_UPDATE = 0x40000000,
+            PAGE_ENCLAVE_THREAD_CONTROL = 0x80000000,
+            PAGE_REVERT_TO_FILE_MAP = 0x80000000
+        }
+
+        [Flags]
+        public enum MemoryFlags : uint
+        {
+            MEM_COMMIT = 0x00001000,
+            MEM_RESERVE = 0x00002000,
+            MEM_DECOMMIT = 0x00004000,
+            MEM_RELEASE = 0x00008000,
+            MEM_FREE = 0x00010000,
+            MEM_PRIVATE = 0x00020000,
+            MEM_MAPPED = 0x00040000,
+            MEM_RESET = 0x00080000,
+            MEM_TOP_DOWN = 0x00100000,
+            MEM_WRITE_WATCH = 0x00200000,
+            MEM_PHYSICAL = 0x00400000,
+            MEM_ROTATE = 0x00800000,
+            MEM_DIFFERENT_IMAGE_BASE_OK = 0x00800000,
+            MEM_RESET_UNDO = 0x01000000,
+            MEM_TYPE = 0x01000000,
+            MEM_LARGE_PAGES = 0x20000000,
+            MEM_4MB_PAGES = 0x80000000,
+            MEM_64K_PAGES = (MEM_LARGE_PAGES | MEM_PHYSICAL)
+        }
+
+        [Flags]
+        public enum ProcessAccessFlags : uint
+        {
+            All = 0x001F0FFF,
+            Terminate = 0x00000001,
+            CreateThread = 0x00000002,
+            VirtualMemoryOperation = 0x00000008,
+            VirtualMemoryRead = 0x00000010,
+            VirtualMemoryWrite = 0x00000020,
+            DuplicateHandle = 0x00000040,
+            CreateProcess = 0x000000080,
+            SetQuota = 0x00000100,
+            SetInformation = 0x00000200,
+            QueryInformation = 0x00000400,
+            QueryLimitedInformation = 0x00001000,
+            Synchronize = 0x00100000
+        }
+
+        [StructLayout(LayoutKind.Sequential)]
+        public struct MEMORY_BASIC_INFORMATION64 // 48
+        {
+            public UIntPtr BaseAddress; // 8
+            public UIntPtr AllocationBase; // 8
+            public AllocationProtect AllocationProtect; // 4
+            public uint __alignment1; // 4
+            public UIntPtr RegionSize; // 8
+            public MemoryFlags State; // 4
+            public AllocationProtect Protect; // 4
+            public MemoryFlags Type; // 4
+            public uint __alignment2; // 4
+        }
+
+        public struct SYSTEM_INFO
+        {
+            public ushort processorArchitecture;
+            public ushort reserved;
+            public uint pageSize;
+            public UIntPtr minimumApplicationAddress;
+            public UIntPtr maximumApplicationAddress;
+            public UIntPtr activeProcessorMask;
+            public uint numberOfProcessors;
+            public uint processorType;
+            public uint allocationGranularity;
+            public ushort processorLevel;
+            public ushort processorRevision;
+        }
+    }
+}
---
+++ ProcessMemory/ProcessMemory.cs
@@ -0,0 +1,244 @@
+﻿using static ProcessMemory.PInvoke;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ProcessMemory
+{
+    public class ProcessMemory : IDisposable
+    {
+        private SYSTEM_INFO sysInfo;
+        private UIntPtr processHandle = UIntPtr.Zero;
+
+        private static TaskCreationOptions taskOpts = TaskCreationOptions.DenyChildAttach;
+        private static TaskScheduler taskSched = TaskScheduler.Default;
+
+        public ProcessMemory(int pid)
+        {
+            sysInfo = new SYSTEM_INFO();
+            GetSystemInfo(out sysInfo);
+            processHandle = OpenProcess(ProcessAccessFlags.QueryInformation | ProcessAccessFlags.VirtualMemoryRead | ProcessAccessFlags.VirtualMemoryWrite, false, (uint)pid);
+        }
+
+        public Task<byte[]> GetByteArrayAtAsync(ulong offset, int size, CancellationToken cancelToken)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                byte[] returnValue = new byte[size];
+                MEMORY_BASIC_INFORMATION64 memBasicInfo = new MEMORY_BASIC_INFORMATION64();
+                IntPtr bytesRead = IntPtr.Zero;
+
+                VirtualQueryEx(processHandle, new UIntPtr(offset), out memBasicInfo, new UIntPtr(48));
+
+                bool hasAnyRead = memBasicInfo.Protect.HasFlag(AllocationProtect.PAGE_READONLY) || memBasicInfo.Protect.HasFlag(AllocationProtect.PAGE_READWRITE) || memBasicInfo.Protect.HasFlag(AllocationProtect.PAGE_EXECUTE_READ) || memBasicInfo.Protect.HasFlag(AllocationProtect.PAGE_EXECUTE_READWRITE);
+                if (hasAnyRead && memBasicInfo.State.HasFlag(MemoryFlags.MEM_COMMIT))
+                {
+                    bool success = ReadProcessMemory(processHandle, offset, returnValue, size, out bytesRead);
+                    int win32Error = Marshal.GetLastWin32Error();
+                }
+
+                return returnValue;
+            }, cancelToken, taskOpts, taskSched);
+        }
+
+        public Task<int> SetByteArrayAtAsync(ulong offset, byte[] data, CancellationToken cancelToken)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                MEMORY_BASIC_INFORMATION64 memBasicInfo = new MEMORY_BASIC_INFORMATION64();
+                IntPtr bytesWritten = IntPtr.Zero;
+
+                VirtualQueryEx(processHandle, new UIntPtr(offset), out memBasicInfo, new UIntPtr(48));
+
+                bool hasAnyRead = memBasicInfo.Protect.HasFlag(AllocationProtect.PAGE_READONLY) || memBasicInfo.Protect.HasFlag(AllocationProtect.PAGE_READWRITE) || memBasicInfo.Protect.HasFlag(AllocationProtect.PAGE_EXECUTE_READ) || memBasicInfo.Protect.HasFlag(AllocationProtect.PAGE_EXECUTE_READWRITE);
+                if (hasAnyRead && memBasicInfo.State.HasFlag(MemoryFlags.MEM_COMMIT))
+                {
+                    bool success = WriteProcessMemory(processHandle, offset, data, data.Length, out bytesWritten);
+                    int win32Error = Marshal.GetLastWin32Error();
+                }
+
+                return bytesWritten.ToInt32();
+            }, cancelToken, taskOpts, taskSched);
+        }
+
+        public async Task<sbyte> GetSByteAtAsync(ulong offset, CancellationToken cancelToken) => (sbyte)(await GetByteArrayAtAsync(offset, 1, cancelToken))[0];
+        public async Task<byte> GetByteAtAsync(ulong offset, CancellationToken cancelToken) => (await GetByteArrayAtAsync(offset, 1, cancelToken))[0];
+        public async Task<short> GetShortAtAsync(ulong offset, CancellationToken cancelToken) => HighPerfBitConverter.ToInt16(await GetByteArrayAtAsync(offset, 2, cancelToken), 0);
+        public async Task<ushort> GetUShortAtAsync(ulong offset, CancellationToken cancelToken) => HighPerfBitConverter.ToUInt16(await GetByteArrayAtAsync(offset, 2, cancelToken), 0);
+        public async Task<int> GetIntAtAsync(ulong offset, CancellationToken cancelToken) => HighPerfBitConverter.ToInt32(await GetByteArrayAtAsync(offset, 4, cancelToken), 0);
+        public async Task<uint> GetUIntAtAsync(ulong offset, CancellationToken cancelToken) => HighPerfBitConverter.ToUInt32(await GetByteArrayAtAsync(offset, 4, cancelToken), 0);
+        public async Task<long> GetLongAtAsync(ulong offset, CancellationToken cancelToken) => HighPerfBitConverter.ToInt64(await GetByteArrayAtAsync(offset, 8, cancelToken), 0);
+        public async Task<ulong> GetULongAtAsync(ulong offset, CancellationToken cancelToken) => HighPerfBitConverter.ToUInt64(await GetByteArrayAtAsync(offset, 8, cancelToken), 0);
+        public async Task<float> GetFloatAtAsync(ulong offset, CancellationToken cancelToken) => BitConverter.ToSingle(await GetByteArrayAtAsync(offset, 4, cancelToken), 0);
+        public async Task<double> GetDoubleAtAsync(ulong offset, CancellationToken cancelToken) => BitConverter.ToDouble(await GetByteArrayAtAsync(offset, 8, cancelToken), 0);
+
+        public async Task<int> SetSByteAtAsync(ulong offset, sbyte value, CancellationToken cancelToken) => await SetByteArrayAtAsync(offset, new byte[1] { (byte)value }, cancelToken);
+        public async Task<int> SetByteAtAsync(ulong offset, byte value, CancellationToken cancelToken) => await SetByteArrayAtAsync(offset, new byte[1] { value }, cancelToken);
+        public async Task<int> SetShortAtAsync(ulong offset, short value, CancellationToken cancelToken) => await SetByteArrayAtAsync(offset, BitConverter.GetBytes(value), cancelToken);
+        public async Task<int> SetUShortAtAsync(ulong offset, ushort value, CancellationToken cancelToken) => await SetByteArrayAtAsync(offset, BitConverter.GetBytes(value), cancelToken);
+        public async Task<int> SetIntAtAsync(ulong offset, int value, CancellationToken cancelToken) => await SetByteArrayAtAsync(offset, BitConverter.GetBytes(value), cancelToken);
+        public async Task<int> SetUIntAtAsync(ulong offset, uint value, CancellationToken cancelToken) => await SetByteArrayAtAsync(offset, BitConverter.GetBytes(value), cancelToken);
+        public async Task<int> SetLongAtAsync(ulong offset, long value, CancellationToken cancelToken) => await SetByteArrayAtAsync(offset, BitConverter.GetBytes(value), cancelToken);
+        public async Task<int> SetULongAtAsync(ulong offset, ulong value, CancellationToken cancelToken) => await SetByteArrayAtAsync(offset, BitConverter.GetBytes(value), cancelToken);
+        public async Task<int> SetFloatAtAsync(ulong offset, float value, CancellationToken cancelToken) => await SetByteArrayAtAsync(offset, BitConverter.GetBytes(value), cancelToken);
+        public async Task<int> SetDoubleAtAsync(ulong offset, double value, CancellationToken cancelToken) => await SetByteArrayAtAsync(offset, BitConverter.GetBytes(value), cancelToken);
+
+        public Task<HashSet<ulong>> ScanMemoryAsync(byte[] searchValue, CancellationToken cancelToken)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                HashSet<ulong> returnValue = new HashSet<ulong>();
+
+                ulong procMinAddress = sysInfo.minimumApplicationAddress.ToUInt64();
+                ulong procMaxAddress = sysInfo.maximumApplicationAddress.ToUInt64();
+
+                MEMORY_BASIC_INFORMATION64 memBasicInfo = new MEMORY_BASIC_INFORMATION64();
+                IntPtr bytesRead = IntPtr.Zero;
+
+                byte[] buffer = null;
+                while (procMinAddress < procMaxAddress)
+                {
+                    VirtualQueryEx(processHandle, new UIntPtr(procMinAddress), out memBasicInfo, new UIntPtr(48));
+
+                    bool hasAnyRead = memBasicInfo.Protect.HasFlag(AllocationProtect.PAGE_READONLY) || memBasicInfo.Protect.HasFlag(AllocationProtect.PAGE_READWRITE) || memBasicInfo.Protect.HasFlag(AllocationProtect.PAGE_EXECUTE_READ) || memBasicInfo.Protect.HasFlag(AllocationProtect.PAGE_EXECUTE_READWRITE);
+                    if (hasAnyRead && memBasicInfo.State.HasFlag(MemoryFlags.MEM_COMMIT))
+                    {
+                        buffer = new byte[memBasicInfo.RegionSize.ToUInt64()];
+                        ReadProcessMemory(processHandle, memBasicInfo.BaseAddress.ToUInt64(), buffer, (int)memBasicInfo.RegionSize, out bytesRead);
+
+                        foreach (int offset in FindIndexesOf(buffer, 0, searchValue))
+                        {
+                            returnValue.Add(memBasicInfo.BaseAddress.ToUInt64() + (ulong)offset);
+                        }
+
+                        buffer = null;
+                        GC.Collect();
+                    }
+
+                    procMinAddress += memBasicInfo.RegionSize.ToUInt64();
+                }
+
+                return returnValue;
+            }, cancelToken, taskOpts, taskSched);
+        }
+
+        public IEnumerable<byte[]> DumpMemory()
+        {
+            ulong procMinAddress = sysInfo.minimumApplicationAddress.ToUInt64();
+            ulong procMaxAddress = sysInfo.maximumApplicationAddress.ToUInt64();
+
+            MEMORY_BASIC_INFORMATION64 memBasicInfo = new MEMORY_BASIC_INFORMATION64();
+            IntPtr bytesRead = IntPtr.Zero;
+
+            while (procMinAddress < procMaxAddress)
+            {
+                VirtualQueryEx(processHandle, new UIntPtr(procMinAddress), out memBasicInfo, new UIntPtr(48));
+
+                bool hasAnyRead = memBasicInfo.Protect.HasFlag(AllocationProtect.PAGE_READONLY) || memBasicInfo.Protect.HasFlag(AllocationProtect.PAGE_READWRITE) || memBasicInfo.Protect.HasFlag(AllocationProtect.PAGE_EXECUTE_READ) || memBasicInfo.Protect.HasFlag(AllocationProtect.PAGE_EXECUTE_READWRITE);
+                if (hasAnyRead && memBasicInfo.State.HasFlag(MemoryFlags.MEM_COMMIT))
+                {
+                    byte[] buffer = new byte[memBasicInfo.RegionSize.ToUInt64()];
+                    ReadProcessMemory(processHandle, memBasicInfo.BaseAddress.ToUInt64(), buffer, (int)memBasicInfo.RegionSize, out bytesRead);
+
+                    yield return buffer;
+                }
+
+                procMinAddress += memBasicInfo.RegionSize.ToUInt64();
+            }
+        }
+
+        public void DumpMemoryToFile(string fileName = "Memory.DMP")
+        {
+            using (FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.ReadWrite))
+            {
+                foreach (byte[] memorySection in DumpMemory())
+                {
+                    fs.Write(memorySection, 0, memorySection.Length);
+                }
+            }
+        }
+
+        private static int FindIndexOf(byte[] array, int start, byte[] sequence)
+        {
+            int end = array.Length - sequence.Length; // past here no match is possible
+            byte firstByte = sequence[0]; // cached to tell compiler there's no aliasing
+
+            while (start < end)
+            {
+                // Check first byte.
+                if (array[start] == firstByte && sequence.Length == 1)
+                {
+                    return start; // Found a winner!
+                }
+                else if (array[start] == firstByte && sequence.Length > 1)
+                {
+                    // First byte matched, perform internal scanning.
+                    for (int offset = 1; offset < sequence.Length; ++offset)
+                    {
+                        if (array[start + offset] != sequence[offset])
+                        {
+                            break; // Mismatch, eject and resume normal searching.
+                        }
+                        else if (offset == sequence.Length - 1)
+                        {
+                            return start; // Found a winner!
+                        }
+                    }
+                }
+                ++start;
+            }
+
+            return -1; // Failed to find a winner.
+        }
+
+        private static IEnumerable<int> FindIndexesOf(byte[] array, int start, byte[] sequence)
+        {
+            int end = array.Length - sequence.Length;
+            int indexOf = 0;
+            while (start < end && (indexOf = FindIndexOf(array, start, sequence)) != -1)
+            {
+                start = indexOf + 1;
+                yield return indexOf;
+            }
+        }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // To detect redundant calls
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects).
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override a finalizer below.
+                // TODO: set large fields to null.
+                CloseHandle(processHandle);
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: override a finalizer only if Dispose(bool disposing) above has code to free unmanaged resources.
+        // ~Memory() {
+        //   // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+        //   Dispose(false);
+        // }
+
+        // This code added to correctly implement the disposable pattern.
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in Dispose(bool disposing) above.
+            Dispose(true);
+            // TODO: uncomment the following line if the finalizer is overridden above.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
