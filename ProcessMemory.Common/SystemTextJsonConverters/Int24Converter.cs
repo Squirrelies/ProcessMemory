dd6f35a3f73c46631274bfbf27d73f8db87bc96e
Travis J. Gutjahr <33809229+Squirrelies@users.noreply.github.com>
2020-09-02 23:08:30 -0500

    ProcessMemory.Common 2.1.1 Added System.Text.Json converters for Int24 and UInt24.

---
+++ ProcessMemory.Common/SystemTextJsonConverters/Int24Converter.cs
@@ -0,0 +1,14 @@
+﻿using ProcessMemory.Common.Types;
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ProcessMemory.Common.SystemTextJsonConverters
+{
+    public class Int24Converter : JsonConverter<Int24>
+    {
+        public override Int24 Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) => reader.GetInt32();
+
+        public override void Write(Utf8JsonWriter writer, Int24 value, JsonSerializerOptions options) => writer.WriteStringValue(value.ToString());
+    }
+}
---
+++ ProcessMemory.Common/SystemTextJsonConverters/UInt24Converter.cs
@@ -0,0 +1,14 @@
+﻿using ProcessMemory.Common.Types;
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace ProcessMemory.Common.SystemTextJsonConverters
+{
+    public class UInt24Converter : JsonConverter<UInt24>
+    {
+        public override UInt24 Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) => reader.GetUInt32();
+
+        public override void Write(Utf8JsonWriter writer, UInt24 value, JsonSerializerOptions options) => writer.WriteStringValue(value.ToString());
+    }
+}
