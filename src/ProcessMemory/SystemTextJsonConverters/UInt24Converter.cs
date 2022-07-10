5bddf673011f52d2b98008c1099d6ac5958936b8
Travis J. Gutjahr <33809229+Squirrelies@users.noreply.github.com>
2022-07-10 15:59:11 -0500

    Re-added the Json converters for Int24 and UInt24. Removed unused files now.

--- src/ProcessMemory/SystemTextJsonConverters/UInt24Converter.cs
+++ src/ProcessMemory/SystemTextJsonConverters/UInt24Converter.cs
@@ -1,14 +1,14 @@
-﻿//using ProcessMemory.Types;
-//using System;
-//using System.Text.Json;
-//using System.Text.Json.Serialization;
+﻿using ProcessMemory.Types;
+using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
-//namespace ProcessMemory.SystemTextJsonConverters
-//{
-//    public class UInt24Converter : JsonConverter<UInt24>
-//    {
-//        public override UInt24 Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) => reader.GetUInt32();
+namespace ProcessMemory.SystemTextJsonConverters
+{
+    public class UInt24Converter : JsonConverter<UInt24>
+    {
+        public override UInt24 Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options) => reader.GetUInt32();
 
-//        public override void Write(Utf8JsonWriter writer, UInt24 value, JsonSerializerOptions options) => writer.WriteNumberValue(value.Value);
-//    }
-//}
+        public override void Write(Utf8JsonWriter writer, UInt24 value, JsonSerializerOptions options) => writer.WriteNumberValue(value.Value);
+    }
+}
